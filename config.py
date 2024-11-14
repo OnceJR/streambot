37457c0c4d70833beebb878edb860ceebe600f98
OnceJR <131706454+OnceJR@users.noreply.github.com>
2024-11-13 23:29:21 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    API_ID = "21660737"         # Reemplaza con tu API_ID de my.telegram.org
+    API_HASH = "610bd34454377eea7619977040c06c66"      # Reemplaza con tu API_HASH de my.telegram.org
+    BOT_TOKEN = "8100674706:AAGzf_JziSNHdJCHHTT4Z9cSWLvF03zF_yU"    # Reemplaza con tu token de @BotFather
+    CHAT_ID = -1733577914        # Reemplaza con el ID de tu canal o grupo
