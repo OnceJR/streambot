f40c49351ea02ce2f7b8d87acbc8547aaf47b2f9
OnceJR <131706454+OnceJR@users.noreply.github.com>
2024-11-14 03:39:55 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    API_ID = "21660737"         # Reemplaza con tu API_ID de my.telegram.org
-    API_HASH = "610bd34454377eea7619977040c06c66"      # Reemplaza con tu API_HASH de my.telegram.org
-    BOT_TOKEN = "8100674706:AAGzf_JziSNHdJCHHTT4Z9cSWLvF03zF_yU"    # Reemplaza con tu token de @BotFather
-    CHAT_ID = -1001733577914        # Reemplaza con el ID de tu canal o grupo
+    API_ID = "21660737"
+    API_HASH = "610bd34454377eea7619977040c06c66"
+    BOT_TOKEN = "8100674706:AAGzf_JziSNHdJCHHTT4Z9cSWLvF03zF_yU"
+    CHAT_IDS = ["@streambot247", -1001733577914]  # Puedes usar nombres de usuario y IDs numéricos
